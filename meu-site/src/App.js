e0f900c64291f7a2520eb6724f6fe5b4ebf8faef
Csa Detec <informaticact@santoagostinho.com.br>
2019-11-26 20:22:40 +0000

    38 conditional rendering

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Member from './Components/Members'
+class App extends Component{
+    constructor(props){
+        super(props)
+        this.state  = {
+            status: false
+        }
+        this.login = this.login.bind(this) 
+        this.logout = this.logout.bind(this)
+    }
 
-function App() {
+    login(){
+        this.setState({status:true})
+    }
+    logout(){
+        this.setState({status:false})
+    }
+    render(){
+        return(
+            <div>
+                {this.state.status ?
+                <div>
+                    <h2>Bem-vindo ao Sistema</h2> 
+                    <button onClick={this.logout}>Sair do Sistema</button>
+                </div> 
+                : 
+                <div>
+                    <h2>Olá visitante, faça o login!</h2>
+                    <button onClick={this.login}>Entrar no Sistema</button>
+                </div>
+                }
+            </div>
+        )
+    }
+}
+
+
+
+
+//import Member from './Components/Members'
+/*
+function App37Events() {
   return (
     <div>
         <Member name="Visitante"/>
     </div>
   );
 }
-
-export default App;
+/** */
+export default App
