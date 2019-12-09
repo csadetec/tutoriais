1217a01f8cdbde1a78f38b062e1ecb00c1fa9b26
Csa Detec <informaticact@santoagostinho.com.br>
2019-12-09 19:44:19 +0000

    end biscoito da sorte

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -6,20 +6,25 @@ class App extends Component{
     constructor(props){
         super(props)
         this.state  = {
-            phrases:[
-                'Siga os bons e aprenda com eles.',
-                 'O bom-senso vale mais do que muito',
-                 'A maior barreira para o sucesso é o medo do fracasso'
-            ]
+            textPhrase: ''
 
         }
+        this.phrases = [
+                'Siga os bons e aprenda com eles.',
+                'O bom-senso vale mais do que muito',
+                'A maior barreira para o sucesso é o medo do fracasso',
+                'Acredite em milagres, mas não dependa deles'
+            ]
 
         this.brokeCookie = this.brokeCookie.bind(this)
       
     }
 
     brokeCookie(){
-        alert('quebrou')
+        //let state = this.state;
+        let random = Math.floor(Math.random() * this.phrases.length)
+       //console.log(random)
+        this.setState({textPhrase: `" ${this.phrases[random]} "`})
     }
 
     render(){
@@ -28,7 +33,7 @@ class App extends Component{
                 <img alt="Of a cookie"
                  src={"http://1.bp.blogspot.com/-734LuPB5HwE/UNMc6ENnliI/AAAAAAAADIE/6VBCFUYESbU/s1600/Fortune-cookie550.jpg"} />
                  <Button name = "Abrir Biscoito" anction={this.brokeCookie} />
-                 <h3 className="textPhrase">Frase</h3>
+                 <h3 className="textPhrase">{this.state.textPhrase}</h3>
             </div>
         )
     }
